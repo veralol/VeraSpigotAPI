af8b608c1595429b1b4e64fc165bbb99046c67fb
curtis <16502091+CurtisOK@users.noreply.github.com>
2023-05-02 22:40:24 +0200

    remove movement and packet handlers for now

--- src/main/java/lol/vera/spigot/VeraSpigotAPI.java
+++ src/main/java/lol/vera/spigot/VeraSpigotAPI.java
@@ -1,6 +1,5 @@
 package lol.vera.spigot;
 
-import lol.vera.spigot.implementation.PacketHandlerImplementation;
 import lol.vera.spigot.implementation.KnockbackImplementation;
 import lombok.Data;
 
@@ -12,8 +11,4 @@ public class VeraSpigotAPI {
 
     private KnockbackImplementation knockback
             = new KnockbackImplementation.DEFAULT();
-
-    private PacketHandlerImplementation packetHandler
-            = new PacketHandlerImplementation.DEFAULT();
-
 }
